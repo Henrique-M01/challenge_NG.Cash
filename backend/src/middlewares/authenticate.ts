983cbe4f4c67baf951b2836faff94d6d3f6a4fbf
henrique <hickm018@gmail.com>
2022-11-15 11:58:47 -0300

    feat: create authenticate middleware

---
+++ backend/src/middlewares/authenticate.ts
@@ -0,0 +1,24 @@
+import { Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import fs from 'fs';
+
+export default function authenticate(req: any, res: Response, next: NextFunction) {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ message: 'Token not found' });
+  }
+
+  const SECRET = fs.readFileSync('./jwt.secret.key', { encoding: 'utf-8' });
+
+  try {
+    const token = authorization.split(' ')[1];
+    const decoded = jwt.verify(token, SECRET);
+
+    req.tokenData = decoded;
+
+    next();
+  } catch (err) {
+    next(err);
+  }
+}
