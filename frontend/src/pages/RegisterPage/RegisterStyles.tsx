af7a552e50565a55c2a6cf58efb541ed79448a4d
henrique <hickm018@gmail.com>
2022-11-16 12:15:25 -0300

    feat: create register page

---
+++ frontend/src/pages/RegisterPage/RegisterStyles.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const RegisterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 380px;
+  margin: 0 auto;
+  margin-top: 90px;
+  padding: 30px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  background-color: #fff;
+
+  h1 {
+    text-align: center;
+  }
+  `;
